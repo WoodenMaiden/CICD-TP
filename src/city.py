5a704a0b09c1a6005a610b133cdec9ea6d31eeff
RedbeanGit <dubois.julien.mail@gmail.com>
2022-05-04 16:33:30 +0200

    feat: Add city
    Signed-off-by: RedbeanGit <dubois.julien.mail@gmail.com>

---
+++ src/city.py
@@ -0,0 +1,9 @@
+class City:
+    def __init__(self, id, department_code, insee_code, zip_code, name, lat, lon):
+        self.id = id
+        self.department_code = department_code
+        self.insee_code = insee_code
+        self.zip_code = zip_code
+        self.name = name
+        self.lat = lat
+        self.lon = lon
